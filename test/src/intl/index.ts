58fb38b92d9c6c0323b6ab14e52a6dacd7a88174
Nander <nander@nander.dev>
2023-08-13 22:35:26 +0200

    update locales for test page

--- test/src/intl/index.ts
+++ test/src/intl/index.ts
@@ -3,15 +3,8 @@ import { createTranslations } from "@customic/intl";
 export const translations = await createTranslations({
   defaultLocale: "en",
   locales: async () =>
-  ({
-    en: {
-      locale: "en",
-      test: "hello",
-      pech: "lol",
-    },
-    de: {
-      locale: "de",
-      test: "hallo",
-    },
-  }) as const,
+    ({
+      en: await import("./en.json"),
+      de: await import("./de.json"),
+    }) as const,
 });
